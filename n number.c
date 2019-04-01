20cefeeaa896dcbee72494758ffdd0d9ff9587d9
vshanmugapriya <48949087+vshanmugapriya@users.noreply.github.com>
2019-04-01 16:23:06 +0530

    Create n number.c

---
+++ n number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i;
+    printf("enter the number");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        printf(" hello world ");
+    }
+}
