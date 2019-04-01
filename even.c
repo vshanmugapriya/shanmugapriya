2887656f0967afa44cbd597f899949d133cd46c7
vshanmugapriya <48949087+vshanmugapriya@users.noreply.github.com>
2019-04-01 15:54:46 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int k;
+  printf("enter the number");
+  scanf("%d",&k);
+  if (k%2==0)
+  {
+      printf("even mumber");
+  }
+  else
+  {
+      printf("odd number");
+  }
+}
